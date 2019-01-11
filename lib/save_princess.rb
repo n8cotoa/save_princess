2e6d85793d159ef6b5d28c62e36dc4b7fc9fce85
Nate Cottle <cottle.nate@gmail.com>
2019-01-10 17:20:48 -0800

    add functionality to displayPathtoPrincess function: iterates over provided grid, outputs a 2d array, the position of p, and position of m.

--- lib/save_princess.rb
+++ lib/save_princess.rb
@@ -1,14 +1,26 @@
 #!/bin/ruby
-
+require 'pry'
 def displayPathtoPrincess(n,grid)
-  return "Working"
+  two_d_grid = []
+  m_position = nil
+  p_position = nil
+  grid.each_with_index do |grid_line, index|
+    two_d_grid.push(split_grid = grid_line.split(''))
+    if split_grid.include?('m')
+      m_position = [index, split_grid.index('m')]
+    end
+    if split_grid.include?('p')
+      p_position = [index, split_grid.index('p')]
+    end
+  end
+
+  binding.pry
 end
 
 m = gets.to_i
-print m
 
 grid = Array.new(m)
-print grid
+
 (0...m).each do |i|
     grid[i] = gets.strip
 end
