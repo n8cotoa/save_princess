4cc85b62cfbef5c884506877a9805a50b4ff4d79
Nate Cottle <cottle.nate@gmail.com>
2019-01-11 08:23:02 -0800

    Add passing tests for receiving user input.

--- spec/save_princess_spec.rb
+++ spec/save_princess_spec.rb
@@ -2,6 +2,10 @@ require('rspec')
 require('save_princess')
 
 describe('#save_princess') do
+    it('will receive user input') do 
+        allow_any_instance_of(Object).to receive(:gets).and_return('input received')
+        expect(gets.chomp).to eq('input received')
+    end
     it('returns position of character') do
         grid = ['---', '-m-', 'p--']
         expect(getCharacterPosition('p', grid)).to(eq([2,0]))
--- spec/save_princess_two_spec.rb
+++ spec/save_princess_two_spec.rb
@@ -2,6 +2,14 @@ require 'rspec'
 require 'save_princess_two'
 
 describe('#save_princess_two') do
+    it('will receive user input') do 
+        allow_any_instance_of(Object).to receive(:gets).and_return('input received')
+        expect(gets.chomp).to eq('input received')
+    end
+    it('returns position of character') do
+        grid = ['---', '-m-', 'p--']
+        expect(getCharacterPosition('p', grid)).to(eq([2,0]))
+    end
     it('will output the next move for the bot') do
         grid = ['-----',
                 '-----',
