bee4d1ad28996364f4f5229b4a1f82c0571967cc
Nate Cottle <cottle.nate@gmail.com>
2019-01-10 20:13:01 -0800

    Add passing test for getCharacterPosition method.

--- lib/save_princess_two.rb
+++ lib/save_princess_two.rb
@@ -1,7 +1,18 @@
 #!/bin/ruby
 
+def getCharacterPosition(char, grid)
+  grid.each_with_index do |grid_line, index|
+    if grid_line.include?(char)
+      return [index, grid_line.index(char)]
+    end
+  end
+end
+
 def nextMove(n,r,c,grid)
-  puts "LEFT\n" 
+    m_position = [r, c]
+    p_position = getCharacterPosition('p', grid)
+
+  
 end
 
 n = gets.to_i
--- spec/save_princess_spec.rb
+++ spec/save_princess_spec.rb
@@ -2,6 +2,10 @@ require('rspec')
 require('save_princess')
 
 describe('#save_princess') do
+    it('returns position of character') do
+        grid = ['---', '-m-', 'p--']
+        expect(getCharacterPosition('p', grid)).to(eq([2,0]))
+    end
     it("returns path of bot to princess") do
         m = 3
         grid = ['---', '-m-', 'p--']
